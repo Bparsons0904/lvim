769bb83bdf0cdedfe4ebf088116c3beacf97964d
Bob Parsons <deadstylebp@gmail.com>
2024-02-21 17:58:13 -0600

    some random config

--- config.lua
+++ config.lua
@@ -2,3 +2,6 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+reload "user.telescope"
+lvim.builtin.nvimtree.setup.view.side = "right"
