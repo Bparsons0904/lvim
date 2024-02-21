ffad2a6825282a037bdf0804eb0ac834c676c87a
Bob Parsons <bob.parsons@seniorvu.com>
2024-02-21 16:25:51 -0600

    base setup

--- config.lua
+++ config.lua
@@ -2,3 +2,76 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.plugins = {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  }
+  ,
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end
+  },
+}
+
+-- Keybindings
+-- Insert mode bindings
+lvim.keys.insert_mode["jk"] = "<Esc>"
+lvim.keys.insert_mode["kj"] = "<Esc>"
+
+-- Normal mode bindings
+-- Save on cmd+s and ctrl+s
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<Cmd-s>"] = ":w<cr>"
+lvim.keys.visual_mode["<C-s>"] = ":w<cr>"
+lvim.keys.visual_mode["<Cmd-s>"] = ":w<cr>"
+lvim.keys.insert_mode["<C-s>"] = "<C-o>:w<cr><Esc>"
+lvim.keys.insert_mode["<Cmd-s>"] = "<C-o>:w<cr><Esc>"
+-- lvim.keys.normal_mode["<F2>"] = ":set spell!<CR>"
+
+-- LunarVim settings
+lvim.format_on_save.enabled = true
+
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+  "go",
+}
+lvim.builtin.treesitter.highlight.enabled = true
+
+-- Vim Settings
+vim.opt.spell = true
+vim.opt.spelllang = 'en_us'
+vim.opt.spelloptions = 'camel'
+
+vim.opt.relativenumber = true
+
+vim.cmd [[autocmd FileType javascriptreact,typescriptreact syntax spell toplevel]]
