f1ae7a5504dc9268efcc81fdd5b640b7ff8a665a
Bob Parsons <bob.parsons@seniorvu.com>
2024-02-22 15:33:18 -0600

    Broke shit

--- config.lua
+++ config.lua
@@ -12,30 +12,114 @@ lvim.plugins = {
     end,
     ft = { "markdown" },
   },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup({
+  --       suggestion = { enabled = false },
+  --       panel = { enabled = false },
+  --     })
+  --   end,
+  -- }
+  -- ,
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   config = function()
+  --     require("copilot_cmp").setup()
+  --   end
+  -- },
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
+    "windwp/nvim-ts-autotag",
     config = function()
-      require("copilot").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      })
+      require("nvim-ts-autotag").setup()
     end,
-  }
-  ,
+  },
   {
-    "zbirenbaum/copilot-cmp",
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    event = "BufRead",
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build =
+    'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+  },
+  -- {
+  --   "tzachar/cmp-tabnine",
+  --   build = "./install.sh",
+  --   dependencies = "hrsh7th/nvim-cmp",
+  --   event = "InsertEnter",
+  -- },
+  {
+    "tpope/vim-surround",
+
+    -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+    -- setup = function()
+    --  vim.o.timeoutlen = 500
+    -- end
+  },
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
     config = function()
-      require("copilot_cmp").setup()
+      require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+        hide_cursor = true,          -- Hide cursor while scrolling
+        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil,       -- Default easing function
+        pre_hook = nil,              -- Function to run before the scrolling animation starts
+        post_hook = nil,             -- Function to run after the scrolling animation ends
+      })
     end
   },
+  {
+    "davidmh/cspell.nvim",
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+        require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+      end, 100)
+    end,
+  }
+
+
 }
+
+-- table.insert(lvim.plugins, {
+--   "zbirenbaum/copilot-cmp",
+--   event = "InsertEnter",
+--   dependencies = { "zbirenbaum/copilot.lua" },
+--   config = function()
+--     vim.defer_fn(function()
+--       require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+--       require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+--     end, 100)
+--   end,
+-- })
+
+lvim.builtin.telescope.on_config_done = function(telescope)
+  pcall(telescope.load_extension, "frecency")
+  pcall(telescope.load_extension, "neoclip")
+  pcall(telescope.load_extension, "fzf-native")
+  -- any other extensions loading
+end
 
 -- Keybindings
 -- Insert mode bindings
 lvim.keys.insert_mode["jk"] = "<Esc>"
 lvim.keys.insert_mode["kj"] = "<Esc>"
+
 
 -- Normal mode bindings
 -- Save on cmd+s and ctrl+s
@@ -46,6 +130,15 @@ lvim.keys.visual_mode["<Cmd-s>"] = ":w<cr>"
 lvim.keys.insert_mode["<C-s>"] = "<C-o>:w<cr><Esc>"
 lvim.keys.insert_mode["<Cmd-s>"] = "<C-o>:w<cr><Esc>"
 -- lvim.keys.normal_mode["<F2>"] = ":set spell!<CR>"
+lvim.keys.normal_mode["<leader>lt"] = ":lua _G.addCurrentWordToCspell()<CR>"
+
+lvim.builtin.which_key.mappings["f"] = {
+  "<cmd>lua require('telescope.builtin').find_files()<CR>", "Find Files"
+}
+
+lvim.builtin.which_key.mappings["sf"] = {
+  "<cmd>lua require('telescope.builtin').git_files()<CR>", "Git Files"
+}
 
 -- LunarVim settings
 lvim.format_on_save.enabled = true
@@ -66,12 +159,162 @@ lvim.builtin.treesitter.ensure_installed = {
   "go",
 }
 lvim.builtin.treesitter.highlight.enabled = true
+lvim.transparent_window = true
 
 -- Vim Settings
-vim.opt.spell = true
+vim.opt.spell = false
 vim.opt.spelllang = 'en_us'
 vim.opt.spelloptions = 'camel'
 
 vim.opt.relativenumber = true
 
-vim.cmd [[autocmd FileType javascriptreact,typescriptreact syntax spell toplevel]]
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  -- { name = "black" },
+  {
+    name = "prettier",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespace
+    -- options such as `--line-width 80` become either `{"--line-width", "80"}` or `{"--line-width=80"}`
+    args = { "--print-width", "100" },
+    ---@usage only start in these filetypes, by default it will attach to all filetypes it supports
+    filetypes = { "javascript", "typescript", "typescriptreact" },
+  },
+  { command = "goimports", filetypes = { "go" } },
+  { command = "gofumpt",   filetypes = { "go" } },
+}
+
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { name = "flake8" },
+--   {
+--     name = "shellcheck",
+--     args = { "--severity", "warning" },
+--   },
+-- }
+
+-- local code_actions = require "lvim.lsp.null-ls.code_actions"
+-- code_actions.setup {
+--   {
+--     name = "proselint",
+--   },
+-- }
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  {
+    name = "cspell",
+    -- Force the severity to be HINT
+    diagnostics_postprocess = function(diagnostic)
+      diagnostic.severity = vim.diagnostic.severity.HINT
+    end,
+
+  },
+}
+
+-- Go Specific Settings
+lvim.builtin.treesitter.ensure_installed = {
+  "go",
+  "gomod",
+}
+
+------------------------
+-- Plugins
+------------------------
+lvim.plugins = {
+  "olexsmir/gopher.nvim",
+  "leoluz/nvim-dap-go",
+}
+
+------------------------
+-- Formatting
+------------------------
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "goimports", filetypes = { "go" } },
+--   { command = "gofumpt", filetypes = { "go" } },
+-- }
+
+-- lvim.format_on_save = {
+--   pattern = { "*.go" },
+-- }
+
+------------------------
+-- Dap
+------------------------
+local dap_ok, dapgo = pcall(require, "dap-go")
+if not dap_ok then
+  return
+end
+
+dapgo.setup()
+
+------------------------
+-- LSP
+------------------------
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls" })
+
+local lsp_manager = require "lvim.lsp.manager"
+lsp_manager.setup("golangci_lint_ls", {
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+})
+
+lsp_manager.setup("gopls", {
+  on_attach = function(client, bufnr)
+    require("lvim.lsp").common_on_attach(client, bufnr)
+    local _, _ = pcall(vim.lsp.codelens.refresh)
+    local map = function(mode, lhs, rhs, desc)
+      if desc then
+        desc = desc
+      end
+
+      vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
+    end
+    map("n", "<leader>Ci", "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies")
+    map("n", "<leader>Ct", "<cmd>GoMod tidy<cr>", "Tidy")
+    map("n", "<leader>Ca", "<cmd>GoTestAdd<Cr>", "Add Test")
+    map("n", "<leader>CA", "<cmd>GoTestsAll<Cr>", "Add All Tests")
+    map("n", "<leader>Ce", "<cmd>GoTestsExp<Cr>", "Add Exported Tests")
+    map("n", "<leader>Cg", "<cmd>GoGenerate<Cr>", "Go Generate")
+    map("n", "<leader>Cf", "<cmd>GoGenerate %<Cr>", "Go Generate File")
+    map("n", "<leader>Cc", "<cmd>GoCmt<Cr>", "Generate Comment")
+    map("n", "<leader>DT", "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test")
+  end,
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+  settings = {
+    gopls = {
+      usePlaceholders = true,
+      gofumpt = true,
+      codelenses = {
+        generate = false,
+        gc_details = true,
+        test = true,
+        tidy = true,
+      },
+    },
+  },
+})
+
+local status_ok, gopher = pcall(require, "gopher")
+if not status_ok then
+  return
+end
+
+gopher.setup {
+  commands = {
+    go = "go",
+    gomodifytags = "gomodifytags",
+    gotests = "gotests",
+    impl = "impl",
+    iferr = "iferr",
+  },
+}
+
+
+_G.addCurrentWordToCspell = function()
+  local current_word = vim.fn.expand("<cword>")
+  local command = string.format("solidgo addword %s", current_word)
+  vim.fn.system(command)
+  print("Added '" .. current_word .. "' to cspell.json")
+end
