413c98cefa9a54469890900bd64360fa3376f137
Bob Parsons <bob.parsons@seniorvu.com>
2024-02-26 09:44:52 -0600

    add harpoon

--- config.lua
+++ config.lua
@@ -77,7 +77,28 @@ lvim.plugins = {
       -- require("scrollbar.handlers.search").setup({}),
       -- })
     end,
-  }
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon.setup()
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+      vim.keymap.set("n", "<leader><leader>a", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<leader><leader>s", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<leader><leader>d", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<leader><leader>f", function() harpoon:list():select(4) end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<leader><leader>j", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<leader><leader>k", function() harpoon:list():next() end)
+    end,
+  },
 }
 
 -- table.insert(lvim.plugins, {
